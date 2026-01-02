8b14b4c31907766841d9af36c0fa2a3865b592f5
SAK70Games <138614585+SAK70Games@users.noreply.github.com>
2026-01-02 17:29:35 -0500

    Update FamilyPhotoViewer 2026-01-02

--- run_family_photo_viewer.bat
+++ run_family_photo_viewer.bat
@@ -4,5 +4,3 @@ if not exist "D:\FamilyPhotoViewer\logs" mkdir "D:\FamilyPhotoViewer\logs"
 
 REM Run the Python script
 python "D:\FamilyPhotoViewer\scripts\family_photo_viewer.py" >> "D:\FamilyPhotoViewer\logs\family_photo_viewer_log.txt" 2>&1
-
-pause
